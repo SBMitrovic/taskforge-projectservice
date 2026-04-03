ab95da0d045b11bc2673211ea5e34d7e3cc856ea
SBMitrovic <122743260+SBMitrovic@users.noreply.github.com>
2026-04-03 23:20:10 +0200

    Application.yml updated

--- src/main/java/com/taskforge/taskforge_projectservice/security/JwtUtil.java
+++ src/main/java/com/taskforge/taskforge_projectservice/security/JwtUtil.java
@@ -3,6 +3,7 @@ package com.taskforge.taskforge_projectservice.security;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
@@ -11,11 +12,13 @@ import java.util.Date;
 @Component
 public class JwtUtil {
 
-    private static final String SECRET = "taskforge-super-secret-key-must-be-at-least-256-bits";
+    @Value("${JWT_SECRET:taskforge-super-secret-key-must-be-at-least-256-bits}")
+    private String secret;
+
     private static final long EXPIRATION = 1000 * 60 * 60 * 24;
 
     private SecretKey getSigningKey() {
-        return Keys.hmacShaKeyFor(SECRET.getBytes());
+        return Keys.hmacShaKeyFor(secret.getBytes());
     }
 
     public String generateToken(String username, String role, Long userId) {
@@ -36,7 +39,7 @@ public class JwtUtil {
     public String extractRole(String token) {
         return getClaims(token).get("role", String.class);
     }
-    
+
     public Long extractUserId(String token) {
         return getClaims(token).get("userId", Long.class);
     }
